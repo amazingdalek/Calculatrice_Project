81e02087012f38e9a463e602c6fb52ee513d8124
Jordan <jordan.slusniak@hotmail.fr>
2020-04-14 18:03:04 +0200

    Ajout RelayCommand + binding nom commande button

--- Calculatrice_project/ViewModel/PaveTactileViewModel.cs
+++ Calculatrice_project/ViewModel/PaveTactileViewModel.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
 
 namespace Calculatrice_project.ViewModel
 {
     public class PaveTactileViewModel : ViewModelBase
     {
-        int numero;
-
+        public int numero;
+        public RelayCommand ButtonCommand1 { get; set; }
         public PaveTactileViewModel()
         {
-            
+            ButtonCommand1 = new RelayCommand(o => Button1Onclick(1));
+        }
+        private void Button1Onclick(object sender)
+        {
+            MessageBox.Show(sender.ToString());
         }
     }
 }
